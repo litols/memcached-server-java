ba0648a4df15008147e6eb5583cb3c19c35a5069
Takahiro Shimizu <t.shimizu@leafgraph.com>
2016-11-13 00:20:02 +0900

    Mainのserver呼び出しを修正

--- src/com/leafgraph/tshimizu/sysdev/memcached/Main.java
+++ src/com/leafgraph/tshimizu/sysdev/memcached/Main.java
@@ -1,13 +1,13 @@
 package com.leafgraph.tshimizu.sysdev.memcached;
 
-import com.leafgraph.tshimizu.sysdev.memcached.presentation.MemstashdServer;
+import com.leafgraph.tshimizu.sysdev.memcached.presentation.MemcachedServer;
 
 public class Main {
 
 
     public static void main(String[] args) {
 	// write your code here
-        MemstashdServer server=new MemstashdServer();
+        MemcachedServer server=new MemcachedServer();
         server.start();
     }
 }
